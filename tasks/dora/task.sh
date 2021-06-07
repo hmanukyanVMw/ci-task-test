88c73ea903020a50b28a03e556f5f67e8e469885
hmanukyan <hmanukyanVMw@vmware.com>
2021-06-07 17:14:01 +0400

    change run script

--- tasks/dora/task.sh
+++ tasks/dora/task.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo 3
+cf api api.${SYSTEM_DOMAIN} --skip-ssl-validation
+cf auth ${CF_USERNAME} ${CF_PASSWORD}
+cf target -o system -s system
+
+pushd dora-src/assets/dora
+  cf push dora
+popd
