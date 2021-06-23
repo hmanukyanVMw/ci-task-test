74e262e5396293efdd4acf38214d2bc3d91954d8
hmanukyan <hmanukyanVMw@vmware.com>
2021-06-23 17:41:53 +0400

    add untested-product

--- Concurse/set-pipelines/set-slack-alert-pipeline/set-demo-pipeline.sh
+++ Concurse/set-pipelines/set-slack-alert-pipeline/set-demo-pipeline.sh
@@ -5,7 +5,8 @@ DOCKER_TAG="latest"
 
 fly --target tutorial set-pipeline --config ./pipeline.yml  \
     --load-vars-from <(lpass show --notes "Shared-PCF Metrics/Catalyst/ci_vars.yml") \
-      --var docker-host=${DOCKER_HOST} \
+    --var appwatch-service-account-json="$(lpass show --notes "bbl-appwatch service account")" \
+    --var docker-host=${DOCKER_HOST} \
     --var docker-tag="${DOCKER_TAG}" \
     --var system-domain=sys.delreyoaks.cf-app.com \
     --var cf-username=admin \
