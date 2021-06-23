3c2ee2e36f85a8b300ab95e3a2d2fb7fe1becb29
hmanukyan <hmanukyanVMw@vmware.com>
2021-06-23 17:25:43 +0400

    add changes

---
+++ Concurse/tasks/set-pipelines/set-slack-alert-pipeline/set-demo-pipeline.sh
@@ -0,0 +1,14 @@
+DOCKER_HOST="10.0.1.5"
+DOCKER_TAG="latest"
+
+#docker-compose up -d
+
+fly --target tutorial set-pipeline --config ./pipeline.yml  \
+    --load-vars-from <(lpass show --notes "Shared-PCF Metrics/Catalyst/ci_vars.yml") \
+      --var docker-host=${DOCKER_HOST} \
+    --var docker-tag="${DOCKER_TAG}" \
+    --var system-domain=sys.delreyoaks.cf-app.com \
+    --var cf-username=admin \
+    --var cf-password=poiO7PNM_Mefna-ZqDqIMtaK3Ilq5Fxl \
+    --pipeline tutorial
+
